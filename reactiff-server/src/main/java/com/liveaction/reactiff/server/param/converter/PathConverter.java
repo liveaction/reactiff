0e3887a7e64c8f54ea10bddd369335b024ce6a42
Simon Daudin <sdaudin@liveaction.com>
2019-03-15 14:41:18 +0100

    feat(server): Added PathConverter

---
+++ reactiff-server/src/main/java/com/liveaction/reactiff/server/param/converter/PathConverter.java
@@ -0,0 +1,22 @@
+package com.liveaction.reactiff.server.param.converter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class PathConverter implements ParamConverter<Path> {
+
+    public static final PathConverter INSTANCE = new PathConverter();
+
+    private PathConverter() {
+    }
+
+    @Override
+    public Path fromString(String s) {
+        return Paths.get(s);
+    }
+
+    @Override
+    public boolean canConvertType(Class<?> clazz) {
+        return clazz == Boolean.class;
+    }
+}
