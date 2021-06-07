dcd3e33217f428ec200e11998a6e0b18aba286b5
Jolan Cubells <jcubells@liveaction.com>
2021-06-07 17:32:41 +0200

    feat(test): trying to reproduce PrematureClosedException in unit test

---
+++ reactiff-server/src/test/java/com/liveaction/reactiff/server/performance/PerformanceTest.java
@@ -0,0 +1,92 @@
+package com.liveaction.reactiff.server.performance;
+
+import com.liveaction.reactiff.api.server.ReactiveHandler;
+import com.liveaction.reactiff.server.DefaultFilters;
+import com.liveaction.reactiff.server.ReactiveHttpServer;
+import com.liveaction.reactiff.server.performance.implementations.PerformanceController;
+import com.liveaction.reactiff.server.rules.WithCodecManager;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
+
+public final class PerformanceTest implements ReactiveHandler {
+    static int i = 0;
+    @ClassRule
+    public static WithCodecManager withCodecManager = new WithCodecManager();
+
+    public static ReactiveHttpServer server;
+    public static HttpClient client;
+
+    @BeforeClass
+    public static void init() {
+        server = ReactiveHttpServer.create()
+                .compress(true)
+                .displayRoutes(true)
+                .protocols(HttpProtocol.HTTP11)
+                .codecManager(withCodecManager.codecManager)
+                .build();
+        server.addReactiveFilter((DefaultFilters.exceptionMapping(throwable -> {
+            if (throwable instanceof IllegalAccessException) return 500;
+            if (throwable instanceof UnsupportedOperationException) return 401;
+            if (throwable instanceof NullPointerException) return 404;
+            return null;
+        })));
+        server.addReactiveHandler(new PerformanceController());
+        server.start();
+
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("http")
+                .maxConnections(500)
+                .build();
+        client = HttpClient.create(connectionProvider)
+                .wiretap(true)
+                .protocol(HttpProtocol.HTTP11)
+                .baseUrl("http://localhost:" + server.port())
+                .headers(httpHeaders -> httpHeaders.set(HttpHeaderNames.ACCEPT, "application/json"));
+    }
+
+    @AfterClass
+    public static void destroy() {
+        server.close();
+    }
+
+    @Test
+    public void no_timeout() {
+        Flux<String> data = Flux.just("str1", "str2").delayElements(Duration.ofMillis(20));
+        client
+                .headers(httpHeaders -> httpHeaders.set("Accept", "application/json"))
+                .post()
+                .uri("/post-timeout")
+                .send(withCodecManager.codecManager.send("application/stream+json", data, String.class))
+                .response(withCodecManager.codecManager.decodeAsFlux(Integer.class)).blockLast();
+    }
+
+    @Test
+    public void timeout_delay_server() {
+        Flux<String> data = Flux.just("str1", "str2");
+        client
+                .headers(httpHeaders -> httpHeaders.set("Accept", "application/json"))
+                .post()
+                .uri("/post-timeout-server")
+                .send(withCodecManager.codecManager.send("application/stream+json", data, String.class))
+                .response(withCodecManager.codecManager.decodeAsFlux(Integer.class)).blockLast();
+    }
+
+    @Test
+    public void timeout_delay_entry() {
+        Flux<String> data = Flux.just("str1", "str2").delayElements(Duration.ofMillis(200));
+        client
+                .headers(httpHeaders -> httpHeaders.set("Accept", "application/json"))
+                .post()
+                .uri("/post-timeout")
+                .send(withCodecManager.codecManager.send("application/stream+json", data, String.class))
+                .response(withCodecManager.codecManager.decodeAsFlux(Integer.class)).blockLast();
+    }
+}
---
+++ reactiff-server/src/test/java/com/liveaction/reactiff/server/performance/implementations/PerformanceController.java
@@ -0,0 +1,48 @@
+package com.liveaction.reactiff.server.performance.implementations;
+
+import com.google.common.reflect.TypeToken;
+import com.liveaction.reactiff.api.server.HttpMethod;
+import com.liveaction.reactiff.api.server.ReactiveHandler;
+import com.liveaction.reactiff.api.server.Request;
+import com.liveaction.reactiff.api.server.annotation.RequestMapping;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class PerformanceController implements ReactiveHandler {
+
+    @RequestMapping(method = HttpMethod.POST, path = "/post-timeout-server")
+    public Flux<Integer> post_timeout_server(Request request) {
+
+        return request.bodyToFlux(new TypeToken<String>() {
+        })
+                .timeout(Duration.ofMillis(100))
+                .flatMap(s -> delayedFlux(Duration.ofMillis(200)), 1);
+    }
+
+    @RequestMapping(method = HttpMethod.POST, path = "/post-timeout")
+    public Flux<Integer> post_timeout(Request request) {
+
+        return request.bodyToFlux(new TypeToken<String>() {
+        })
+                .timeout(Duration.ofMillis(100))
+                .flatMap(s -> delayedFlux(Duration.ofMillis(1)), 1);
+    }
+
+    @RequestMapping(method = HttpMethod.POST, path = "/post-cancel")
+    public Mono<Integer> post_cancel(Request request) {
+
+        return request.bodyToFlux(new TypeToken<String>() {
+        })
+                .next()
+                .map(s ->  1);
+    }
+
+    private Flux<Integer> delayedFlux(Duration delay) {
+        return Flux.just(1)
+                .delayElements(delay);
+    }
+
+}
